7ba351a1c5594b1c52ffcd34580a698d2db9beb9
bartick <69100224+bartick@users.noreply.github.com>
2020-11-15 12:04:14 +0530

    Help command

--- Help.py
+++ Help.py
@@ -13,9 +13,9 @@ class Help(commands.Cog):
 	async def help_command(self, ctx):
 		hel = discord.Embed(title=f"Help for {client.user.name}", description="All Commands are listed below", color=ctx.author.color)
 		hel.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-		hel.add_field(name="ModMail",value="dm\nclose\nsend\na or add\nremove or rn",inline=True)
-		hel.add_field(name="Fun",value="8ball\nguess or gs",inline=True)
-		hel.add_field(name="Search",value="anime\npfp or av or avatar\nwiki",inline=True)
+		hel.add_field(name="ModMail",value="dm\nclose\nsend\na `or` add\nremove `or` rn",inline=True)
+		hel.add_field(name="Fun",value="8ball\nguess `or` gs",inline=True)
+		hel.add_field(name="Search",value="anime\npfp `or` av `or` avatar\nwiki",inline=True)
 		hel.timestamp = datetime.datetime.now()
 		await ctx.send(embed=hel)
 
